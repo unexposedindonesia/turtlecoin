69699d059de2993bc554b53acdd4da55b67a600d
unexposedindonesia <56574722+unexposedindonesia@users.noreply.github.com>
2021-06-30 01:55:10 +0700

    Add files via upload

---
+++ miner.sh
@@ -0,0 +1,3 @@
+@echo off
+./xmrig --donate-level 1 -o turtlecoin.herominers.com:10380 -u TRTLv3cFfV58hY6nGhPiLUjaXXuG6FwYo1PHrLUaQUoKFHBTtJMzDPW9RjSBN6tTQ2EC2aESeH72iYxxeJMZ7CbGCVVzAp7522A -p wik1 -a argon2/chukwav2 -k 
+pause
